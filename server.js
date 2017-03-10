0012b0e6902145bbca22f1c750a10cf2f093f4b1
Nathan Brewer-Davis <10ndavis10@gmail.com>
2017-03-09 21:14:08 -0600

    Complete
    Complete

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 var app = require('./server-config.js');
 
-var port = 4568;
+var port = process.env.PORT || 4568;
 
 app.listen(port);
 
 console.log('Server now listening on port ' + port);
-console.log('server started!!')
+
+//like really, does this work?
